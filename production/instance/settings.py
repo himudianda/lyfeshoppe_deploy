21ff1b4d7bc34adb9fe57fd9ee83eb7c3422e859
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-12-25 17:18:56 -0800

    Modified all files per latest update to production

--- production/instance/settings.py
+++ production/instance/settings.py
@@ -6,9 +6,6 @@ SECRET_KEY = 'nevergivethisoutandmakesureitiscomplexdonotusewhatyouseehereseriou
 DEBUG = False
 LOG_LEVEL = 'INFO'
 
-MAIL_USERNAME = 'you@example.com'
-MAIL_PASSWORD = 'thebestpasswordyouevermade'
-
 db_uri = 'postgresql://lyfeshoppe:pass@postgres:5432/lyfeshoppe'
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_POOL_SIZE = 25
@@ -19,13 +16,19 @@ CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_REDIS_MAX_CONNECTIONS = 25
 
-STRIPE_SECRET_KEY = 'XXX'
-STRIPE_PUBLISHABLE_KEY = 'XXX'
+# Mail settings.
+MAIL_USERNAME = 'invincible999@gmail.com'
+MAIL_PASSWORD = 'qaWSer12'
 
-TWITTER_CONSUMER_KEY = 'XXX'
-TWITTER_CONSUMER_SECRET = 'XXX'
-TWITTER_ACCESS_TOKEN = 'XXX'
-TWITTER_ACCESS_SECRET = 'XXX'
+# Stripe settings.
+STRIPE_SECRET_KEY = 'sk_test_qiyNl7uKGcZur3kBFbZ8CHYm'
+STRIPE_PUBLISHABLE_KEY = 'pk_test_dwxVrnUVeaWrsUJEKYH3euhB'
+
+# Twitter settings.
+TWITTER_CONSUMER_KEY = 'O3ULXdFEzQO7sn9gqLxKOcl3w'
+TWITTER_CONSUMER_SECRET = 'CNXpCbxyyWU3iy9JlO0IqpZuTAZyQnUVFczn35lV1HCAxsGyKr'
+TWITTER_ACCESS_TOKEN = '3972337814-uZ17beJg9IM4SBnunN7GGXCZofpmZe9LVQqqX0E'
+TWITTER_ACCESS_SECRET = 'Oiw3ghLGQz6PNEJi8VsCwlpSXZHB1uO3WbTpkSO9voEFe'
 
 BROADCAST_PUBLIC_URL = 'https://lyfeshoppe.com/faye/stream'
 BROADCAST_INTERNAL_URL = 'http://faye:4242/stream'
